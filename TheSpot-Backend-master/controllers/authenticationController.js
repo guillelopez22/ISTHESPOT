25dd3641fea57353b34acb23d5615cdb3016c565
Diego <dam_900@hotmail.com>
2019-08-30 11:09:03 -0600

    npm install plz

--- TheSpot-Backend-master/controllers/authenticationController.js
+++ TheSpot-Backend-master/controllers/authenticationController.js
@@ -4,48 +4,56 @@ var usuario = require('../schemas/usuario.js');
 var bcrypt = require('bcrypt');
 
 exports.login = {
-    auth: false,
-    validate: {
-      payload: {
-        usuario: joi.string().required(),
-        contrasena: joi.string().min(2).max(200).required()
-      }
-    },
-    handler: function(request, reply) {
-      console.log(request.payload.contrasena);
-      console.log("entro a login")
-      usuario.find({usuario: request.payload.usuario}, function(err, usuario){
-
-        console.log('usuario: ', request.payload.usuario, 'usuario', usuario)
-        if(err){
-          console.log(err);
-          return reply.response(boom.notAcceptable('Error Executing Query'));
-        }
-        if(usuario.length > 0){
-          bcrypt.compare(request.payload.contrasena, usuario[0].contrasena, function(err, res){
-            console.log("esto es el ReSSS:"+res);
-            if(err)
-                return reply.response(boom.unauthorized('ERROR'));
-            if(res){
-              console.log('before setting cookie');
-              request.cookieAuth.set(usuario[0]);
-              console.log('after setting cookie')
-              return reply.response({usuario: usuario[0].usuario, scope: usuario[0].scope, status:"ok"});
-            }else{
-              return reply(boom.unauthorized('Wrong contrasena'))
-            }
-          });
-        }
-      });
+  auth: false,
+  validate: {
+    payload: {
+      usuario: joi.string().required(),
+      contrasena: joi.string().min(2).max(200).required()
     }
+  },
+  handler: function (request, reply) {
+    console.log(request.payload.contrasena);
+    console.log("entro a login")
+    usuario.find({ usuario: request.payload.usuario }, function (err, usuario) {
+
+      console.log('usuario: ', request.payload.usuario, 'usuario', usuario)
+      if (err) {
+        console.log(err);
+        return reply.response(boom.notAcceptable('Error Executing Query'));
+      }
+      console.log("este es el usuario :", usuario);
+      if (usuario.length > 0) {
+        bcrypt.compare(request.payload.contrasena, usuario[0].contrasena, function (err, res) {
+          console.log("esto es el ReSSS:" + res);
+          if (err) {
+            console.log(err);
+            return reply.response(('ERROR')).code(401);
+          }
+          if (res) {
+            console.log('before setting cookie');
+            request.cookieAuth.set({ usuario: usuario });
+            request.cookieAuth.set(usuario[0]);
+            console.log('after setting cookie')
+            return reply.response({ usuario: usuario[0].usuario, scope: usuario[0].scope, status: "ok" });
+          } else {
+            return reply.response(('ERROR')).code(401);
+          }
+        });
+      }
+      else {
+        return reply.response(('Usuario no existe')).code(401);
+      }
+    });
+    return null;
+  }
 };
 exports.logout = {
-    // auth: {
-    //   mode:'required',
-    //   strategy:'session'
-    // },
-    handler: function(request, reply) {
-      request.cookieAuth.clear();
-      return reply('Logout Successful!');
-    }
-  };
+  // auth: {
+  //   mode:'required',
+  //   strategy:'session'
+  // },
+  handler: function (request, reply) {
+    request.cookieAuth.clear();
+    return reply('Logout Successful!');
+  }
+};
