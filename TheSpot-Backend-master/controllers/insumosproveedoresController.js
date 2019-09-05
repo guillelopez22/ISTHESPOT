d5229348d5415dc8c0a38bc1a7c1c984f287a7b5
guillelopez22 <guillelopez_13@hotmail.com>
2019-09-05 15:48:49 -0600

    insumo proveedor

--- TheSpot-Backend-master/controllers/insumosproveedoresController.js
+++ TheSpot-Backend-master/controllers/insumosproveedoresController.js
@@ -1,6 +1,6 @@
 var insumoproveedor = require('../schemas/insumoproveedor.js');
 var mongoose = require('mongoose');
-
+const boom = require('boom');
 exports.getInsumosProveedores = {
 
     handler: function (request, reply) {
@@ -10,109 +10,149 @@ exports.getInsumosProveedores = {
 }
 exports.getInsumoProveedorId = {
 
-    handler: function (request, reply) {
-        this.envio = "w"
-        insumoproveedor.findOne({ '_id': request.params._id }, function (err, InsumoProveedor) {
-            if (!err && InsumoProveedor) {
-                this.envio = (InsumoProveedor);
-            } else if (!err) {
-                this.envio = (boom.notFound());
-            } else if (err) {
-                this.envio = (boom.wrap(err, 'InsumoProveedor not found'));
-            }
-        });
-        return envio;
+    handler: async function (request, reply) {
+        try {
+            var Insumo = await insumoproveedor.findById(request.params._id).exec();
+            return reply.response(Insumo);
+          } catch (error){
+            throw boom.notFound();
+          }
+        // this.envio = "w"
+        // insumoproveedor.findOne({ '_id': request.params._id }, function (err, InsumoProveedor) {
+        //     if (!err && InsumoProveedor) {
+        //         this.envio = (InsumoProveedor);
+        //     } else if (!err) {
+        //         this.envio = (boom.notFound());
+        //     } else if (err) {
+        //         this.envio = (boom.wrap(err, 'InsumoProveedor not found'));
+        //     }
+        // });
+        // return envio;
     }
 }
 
 exports.getInsumoProveedorInsumo = {
 
-    handler: function (request, reply) {
-        this.envio = "w"
-        insumoproveedor.find({ 'idInsumo': request.params.idInsumo }, function (err, InsumosProveedores) {
-            if (!err && InsumosProveedores) {
-                this.envio = (InsumosProveedores);
-            } else if (!err) {
-                this.envio = (boom.notFound());
-            } else if (err) {
-                this.envio = (boom.wrap(err, 'InsumosProveedores not found'));
-            }
-        }); return envio;
+    handler: async function (request, reply) {
+        try {
+            var Insumo = await insumoproveedor.find({'idInsumo': request.params.idInsumo}).exec();
+            return reply.response(Insumo);
+          } catch (error){
+            throw boom.notFound();
+          }
+        // this.envio = "w"
+        // insumoproveedor.find({ 'idInsumo': request.params.idInsumo }, function (err, InsumosProveedores) {
+        //     if (!err && InsumosProveedores) {
+        //         this.envio = (InsumosProveedores);
+        //     } else if (!err) {
+        //         this.envio = (boom.notFound());
+        //     } else if (err) {
+        //         this.envio = (boom.wrap(err, 'InsumosProveedores not found'));
+        //     }
+        // }); return envio;
     }
 }
 
 exports.getInsumoProveedorProveedor = {
 
-    handler: function (request, reply) {
-        this.envio = "h"
-        insumoproveedor.find({ 'idProveedor': request.params.idProveedor }, function (err, InsumosProveedores) {
-            if (!err && InsumosProveedores) {
-                this.envio = (InsumosProveedores);
-            } else if (!err) {
-                this.envio = (boom.notFound());
-            } else if (err) {
-                this.envio = (boom.wrap(err, 'InsumosProveedores not found'));
-            }
-        }); return envio
+    handler: async function (request, reply) {
+        try {
+            var Insumo = await insumoproveedor.find({'idProveedor': request.params.idProveedor}).exec();
+            return reply.response(Insumo);
+          } catch (error){
+            throw boom.notFound();
+          }
+        // this.envio = "h"
+        // insumoproveedor.find({ 'idProveedor': request.params.idProveedor }, function (err, InsumosProveedores) {
+        //     if (!err && InsumosProveedores) {
+        //         this.envio = (InsumosProveedores);
+        //     } else if (!err) {
+        //         this.envio = (boom.notFound());
+        //     } else if (err) {
+        //         this.envio = (boom.wrap(err, 'InsumosProveedores not found'));
+        //     }
+        // }); return envio
     }
 }
 
 exports.modifyInsumoProveedor = {
 
-    handler: function (request, reply) {
-        this.envio = "w"
-        insumoproveedor.update(
-            { '_id': request.params._id },
-            {
-                $set:
-                {
-                    idInsumo: request.payload.idInsumo,
-                    idProveedor: request.payload.idProveedor,
-                }
-            }, function (err) {
-                if (err) {
-                    this.envio = (boom.wrap(err, 'InsumoProveedor not found'));
-                } else {
-                    this.envio = ('updated succesfully');
-                }
-            }
-        ); return envio;
+    handler: async function (request, reply) {
+        try {
+            var result = await insumo.insumoproveedor(request.params._id, request.payload, { new: true });
+            return reply.response(result);
+          } catch (error) {
+              throw boom.badRequest();
+          }
+        // this.envio = "w"
+        // insumoproveedor.update(
+        //     { '_id': request.params._id },
+        //     {
+        //         $set:
+        //         {
+        //             idInsumo: request.payload.idInsumo,
+        //             idProveedor: request.payload.idProveedor,
+        //         }
+        //     }, function (err) {
+        //         if (err) {
+        //             this.envio = (boom.wrap(err, 'InsumoProveedor not found'));
+        //         } else {
+        //             this.envio = ('updated succesfully');
+        //         }
+        //     }
+        // ); return envio;
     }
 }
 exports.deleteInsumoProveedor = {
 
-    handler: function (request, reply) {
-        this.envio = "w"
-        insumoproveedor.deleteMany({ 'idInsumo': request.params._id }, function (err, InsumoProveedor) {
-            if (err) {
-                this.envio = (boom.badRequest("Could not delete InsumoProveedor"));
-            } else if (!err && InsumoProveedor) {
-                //InsumoProveedor.remove();
-                this.envio = ('InsumoProveedor deleted succesfully');
-            } else if (!err) {
-                this.envio = (boom.notFound());
-            }
-        }); return envio;
+    handler: async function (request, reply) {
+        try {
+            var result = await insumoproveedor.findByIdAndDelete(request.params._id);
+            return reply.response({success: true});
+        } catch (error) {
+          throw boom.badRequest();
+        }
+        // this.envio = "w"
+        // insumoproveedor.deleteMany({ 'idInsumo': request.params._id }, function (err, InsumoProveedor) {
+        //     if (err) {
+        //         this.envio = (boom.badRequest("Could not delete InsumoProveedor"));
+        //     } else if (!err && InsumoProveedor) {
+        //         //InsumoProveedor.remove();
+        //         this.envio = ('InsumoProveedor deleted succesfully');
+        //     } else if (!err) {
+        //         this.envio = (boom.notFound());
+        //     }
+        // }); return envio;
     }
 }
 exports.createInsumoProveedor = {
 
-    handler: function (request, reply) {
-        this.envio = "w"
-        var newInsumoProveedor = new insumoproveedor({
-            idInsumo: request.payload.idInsumo,
-            idProveedor: request.payload.idProveedor,
-        });
-        newInsumoProveedor.save(function (err) {
-            if (!err) {
-                this.envio = {
-                    success: true
-                };
-            } else {
-                this.envio = {
-                    success: false
-                }
-            }
-        }); return envio;
+    handler: async function (request, reply) {
+        try {
+            var newInsumoProveedor = new insumoproveedor({
+                idInsumo: request.payload.idInsumo,
+                idProveedor: request.payload.idProveedor,
+            });
+            var result = await newInsumoProveedor.save();
+            return reply.response({success: true, insumoProveedor: result});
+        } catch(error) {
+             throw boom.badRequest();
+        }
+        // this.envio = "w"
+        // var newInsumoProveedor = new insumoproveedor({
+        //     idInsumo: request.payload.idInsumo,
+        //     idProveedor: request.payload.idProveedor,
+        // });
+        // newInsumoProveedor.save(function (err) {
+        //     if (!err) {
+        //         this.envio = {
+        //             success: true
+        //         };
+        //     } else {
+        //         this.envio = {
+        //             success: false
+        //         }
+        //     }
+        // }); return envio;
     }
 }
