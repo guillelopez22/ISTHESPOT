3375909a9c97b9e8892ebaa767132e1d7b5a60c3
Erickmartinez2 <erickmartinez2203@gmail.com>
2019-09-12 09:25:02 -0600

    Arreglado

--- TheSpot-Backend-master/controllers/combosController.js
+++ TheSpot-Backend-master/controllers/combosController.js
@@ -2,17 +2,17 @@ var combo = require('../schemas/combo.js');
 var mongoose = require('mongoose');
 const boom = require('boom');
 exports.getCombos = {
-  handler: function(request, reply){
+  handler: function (request, reply) {
     var combos = combo.find({});
-    return(combos);
+    return (combos);
   }
 }
 exports.getComboId = {
-  handler : async function(request, reply){
+  handler: async function (request, reply) {
     try {
       var Combo = await combo.findById(request.params._id).exec();
       return reply.response(Combo);
-    } catch (error){
+    } catch (error) {
       throw boom.notFound();
     }
     // this.envio = "w";
@@ -28,12 +28,12 @@ exports.getComboId = {
   }
 }
 exports.getComboName = {
-  
-  handler : async function(request, reply){
+
+  handler: async function (request, reply) {
     try {
-      var Combo = await combo.find({'nombre': request.params.nombre}).exec();
+      var Combo = await combo.find({ 'nombre': request.params.nombre }).exec();
       return reply.response(Combo);
-    } catch (error){
+    } catch (error) {
       throw boom.notFound();
     }
     // this.envio = "w";
@@ -49,12 +49,12 @@ exports.getComboName = {
   }
 }
 exports.getComboPrecio = {
-  
-  handler : async function(request, reply){
+
+  handler: async function (request, reply) {
     try {
-      var Combo = await combo.find({'precio': request.params.precio}).exec();
+      var Combo = await combo.find({ 'precio': request.params.precio }).exec();
       return reply.response(Combo);
-    } catch (error){
+    } catch (error) {
       throw boom.notFound();
     }
     // this.envio = "w";
@@ -70,69 +70,40 @@ exports.getComboPrecio = {
   }
 }
 exports.modifyCombo = {
-  
-  handler: async function(request, reply){
+  handler: async function (request, reply) {
     try {
       var result = await combo.findByIdAndUpdate(request.params._id, request.payload, { new: true });
       return reply.response(result);
     } catch (error) {
-        throw boom.badRequest();
+      throw boom.badRequest();
     }
-    // this.envio = "w";
-    // combo.update(
-    //   {'_id': request.params._id},
-    //   {$set:
-    //     {
-    //       nombre : request.payload.nombre,
-    //       descripcion : request.payload.descripcion,
-    //       precio : request.payload.precio
-    //     }
-    //   }, function(err){
-    //     if(err){
-    //       this.envio = (boom.wrap(err, 'Combo not found'));
-    //     }else{
-    //       this.envio = ('updated succesfully');
-    //     }
-    //   }
-    // );return envio;
   }
 }
 exports.deleteCombo = {
- 
-  handler: async function(request, reply){
+  handler: async function (request, reply) {
     try {
       var result = await combo.findByIdAndDelete(request.params._id);
-      return reply.response({success: true});
-  } catch (error) {
-    throw boom.badRequest();
-  }
-    // this.envio = "w";
-    // combo.findOne({'_id' : request.params._id}, function(err, Combo){
-    //   if(err){
-    //     this.envio =(boom.badRequest("Could not delete Combo"));
-    //   }else if(!err && Combo){
-    //     Combo.remove();
-    //     this.envio =('Combo deleted succesfully');
-    //   }else if(!err){
-    //     this.envio =(boom.notFound());
-    //   }
-    // });return envio;
+      return reply.response({ success: true });
+    } catch (error) {
+      throw boom.badRequest();
+    }
   }
 }
 exports.createCombo = {
-  
-  handler: async function(request, reply){
+
+  handler: async function (request, reply) {
     try {
       var newCombo = new combo({
-        nombre : request.payload.nombre,
-        descripcion : request.payload.descripcion,
-        precio : request.payload.precio
+        nombre: request.payload.nombre,
+        descripcion: request.payload.descripcion,
+        precio: request.payload.precio,
+        imagen: request.payload.imagen
       });
       var result = await newCombo.save();
-      return reply.response({success: true, combo: result});
-  } catch (error) {
-     throw boom.badRequest();
-  }
+      return reply.response({ success: true, combo: result });
+    } catch (error) {
+      throw boom.badRequest();
+    }
     // this.envio = "w";
     // var newCombo = new combo({
     //   nombre : request.payload.nombre,
